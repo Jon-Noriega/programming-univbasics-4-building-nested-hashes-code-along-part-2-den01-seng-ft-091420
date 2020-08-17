a98ef7d1a03fa9f4ec1ee9df48b46b8530650798
Jon Noriega <noriega331@gmail.com>
2020-08-17 19:13:29 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -44,9 +44,9 @@ def add_characters
          {name: "Nurse", age: "44", attitude: "worried"}
          ]
       }
-      :additional_characters => [
+      {:additional_characters => [
         {name: "Prince Escalus"},
         {name: "Apothecary"}
-        ]
+        ]}
    epic_tragedy
 end
